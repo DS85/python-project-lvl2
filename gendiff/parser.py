8e0a1250fe1c25d1a9a26b21067c8323382826a7
DS85 <dimon85@gmail.com>
2022-05-12 09:59:54 +0400

    step2: command line args added

--- gendiff/parser.py
+++ gendiff/parser.py
@@ -5,5 +5,6 @@ def parser():
         description='Compares two configuration files and shows a difference.')
     parser.add_argument('first_file', type=str)
     parser.add_argument('second_file', type=str)
+    parser.add_argument('-f', '--format', help='set format of output')
     args = parser.parse_args()
     print(args)
