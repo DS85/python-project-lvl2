7544f9935c3a23d0f9e4e134182dd4f86e04aaee
DS85 <dimon85@gmail.com>
2022-05-23 19:23:08 +0400

    JSON fix

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -7,7 +7,7 @@ from gendiff.generate_diff import generate_diff
 def main():
     file1, file2, format = parser()
     result = generate_diff(file1, file2, format)
-    print(result.replace('"', ''))
+    print(result)
 
 
 if __name__ == '__main__':
